ecdb1311c2457bb6a8b24219194fe684606aeae5
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2020-08-17 11:50:00 +0300

    start Validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,8 +13,8 @@ export class LoginComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      'username': [''],
-      'password': ['']
+      'username': ['', Validators.required],
+      'password': ['', Validators.required]
     });
   }
 
